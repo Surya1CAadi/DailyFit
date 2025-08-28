7910f205e0b37d322413c609b26cb8c3aa81972e
Surya1CAadi <aadityamaurya97@gmail.com>
2025-08-29 00:23:49 +0530

    updating user and owner calls, adding routing

--- Backend/app.js
+++ Backend/app.js
@@ -6,6 +6,7 @@ const mongoSanitize = require("express-mongo-sanitize"); //sanitize the mongo in
 const xss = require("xss-clean"); //removes malicious code from input
 const cors = require("cors"); //prevents cors blockage
 const cookieParser = require("cookie-parser");
+const otherController = require("./controllers/otherController");
 const rateLimit = require('express-rate-limit')
 
 
@@ -52,6 +53,22 @@ app.get("/test", async (req, res, next) => {
 });
 
 //defining routers
+// todo: routes here
+const userRouter = require("./routes/userRouters");
+const gymRouter = require("./routes/gymRouters");
+const ownerRouter = require("./routes/ownerRouters");
+app.use("/user", userRouter);
+app.use("/gym", gymRouter);
+app.use("/owner", ownerRouter);
+
+//can contact using form limit number of times per hour
+// noinspection JSCheckFunctionSignatures
+app.post("/contact", rateLimit({
+    windowMs: 60 * 60 * 1000,
+    limit: 4,
+    message: "Rate limit reached. Please try again after an hour."
+}), otherController.sendContactEmail);
+
 
 app.post("/clearSiteData",(req,res,next)=>{
     res.setHeader("Clear-Site-Data", "\"*\"").json({
@@ -60,7 +77,16 @@ app.post("/clearSiteData",(req,res,next)=>{
     });
 });
 
+//for undefined routs
+const AppError = require("./util/appError");
+app.all("*", (req, res, next) => {
+    next(
+        new AppError(`Can't find ${req.originalUrl} on Duck server!`, 404)
+    );
+});
+
 //in case of operational error this middleware function will be called to return relevant error message
+const globalErrorController = require("./controllers/errorController");
 app.use(globalErrorController);
 
 module.exports = app;
--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -5,18 +5,31 @@ import { ToastContainer, Zoom } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
 import Root from "./pages/Root.jsx";
 import LoginPage from "./pages/LoginPage.jsx";
+import GymListingPage, { loadGyms } from "./pages/GymListingPage.jsx";
+import GymInfoPage, { loadGymInfo } from "./pages/GymInfoPage.jsx";
+import ErrorPage from "./pages/ErrorPage.jsx";
 import LandingPage from "./pages/LandingPage.jsx";
+import OwnerHomePage, { loadHomePageData } from "./pages/OwnerHomePage.jsx";
+import OwnerPastRecord, { getBookings } from "./pages/OwnerPastRecord.jsx";
+import OrdersDetail, { getOrderDetails } from "./pages/OrdersDetail.jsx";
 import ContactUsPage from "./pages/ContactUsPage.jsx";
+import PaymentPage from "./pages/paymentPage.jsx";
+import GymSignup from "./pages/GymSignup.jsx";
 import ProfilePage, { loadBookings } from "./pages/ProfilePage.jsx";
+import OwnerRoot from "./pages/OwnerRoot.jsx";
+import OwnerLoginPage from "./pages/OwnerLoginPage.jsx";
 import ErrorPageOwner from "./pages/ErrorPageOwner.jsx";
 import ForgotPasswordUser from "./pages/ForgotPasswordUser.jsx";
 import ForgotPasswordUserReset from "./pages/ForgotPasswordUserReset.jsx";
+import ForgotPasswordOwner from "./pages/ForgotPasswordOwner.jsx";
+import ForgotPasswordOwnerReset from "./pages/ForgotPasswordOwnerReset.jsx";
 
 function App() {
   const router = createBrowserRouter([
     {
       path: "/",
       element: <Root />,
+      errorElement: <ErrorPage />,
       children: [
         {
           path: "",
@@ -35,6 +48,20 @@ function App() {
           element: <LoginPage />,
         },
         {
+          path: "gymList/:city",
+          loader: loadGyms,
+          element: <GymListingPage />,
+        },
+        {
+          path: "gymInfo/:gymId",
+          loader: loadGymInfo,
+          element: <GymInfoPage />,
+        },
+        {
+          path: "pay",
+          element: <PaymentPage />,
+        },
+        {
           path: "contactuspage",
           element: <ContactUsPage />,
         },
@@ -51,6 +78,46 @@ function App() {
           path: "forgotPassword/:token",
           element: <ForgotPasswordUserReset />,
         },
+        {
+          path: "ordersdetail/:orderId",
+          element: <OrdersDetail />,
+          loader: getOrderDetails,
+        },
+      ],
+    },
+    {
+      path: "/owner",
+      element: <OwnerRoot />,
+      errorElement: <ErrorPageOwner />,
+      children: [
+        {
+          path: "signup",
+          element: <GymSignup />,
+        },
+        {
+          path: "login",
+          element: <OwnerLoginPage />,
+        },
+        {
+          path: "checkin",
+          element: <OwnerHomePage />,
+          loader: loadHomePageData,
+        },
+        {
+          path: "pastrecords",
+          element: <OwnerPastRecord />,
+          loader: getBookings,
+        },
+        {
+          path: "contactus",
+          element: <ContactUsPage />,
+        },{
+          path: "forgotPassword",
+          element: <ForgotPasswordOwner/>
+        },{
+          path: "forgotPassword/:token",
+          element: <ForgotPasswordOwnerReset />,
+        }
       ],
     },
   ]);
